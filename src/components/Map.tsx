c58436071e3257c8bc7cf0b42815681d1a92070d
Jean Carlo <jeancarlodev@gmail.com>
2023-12-19 15:25:15 -0300

    feat: test adding new markers

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useRef, useState } from "react";
 import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup, Rectangle } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Rectangle, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Icon } from "leaflet";
 import MarkerIcon from "../../node_modules/leaflet/dist/images/marker-icon.png";
@@ -22,16 +22,59 @@ export default function Map() {
       <div className="mx-auto">
         <MapContainer center={mapCenter} zoom={13} scrollWheelZoom={true}>
           <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={mapCenter} icon={new Icon({ iconRetinaUrl: MarkerIcon.src, iconUrl: MarkerIcon.src, shadowUrl: require("leaflet/dist/images/marker-shadow.png") })}>
+          <Markers />
+
+          {/* <Marker position={mapCenter} icon={new Icon({ iconRetinaUrl: MarkerIcon.src, iconUrl: MarkerIcon.src, shadowUrl: require("leaflet/dist/images/marker-shadow.png") })}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
-          </Marker>
+          </Marker> */}
         </MapContainer>
       </div>
     </div>
   );
 }
+
+function Markers() {
+  const map = useMapEvents({
+    click(e) {
+      console.log(e);
+      //  markers.push(e.latlng);
+      //setMarkers((prevValue) => [...prevValue, e.latlng]);
+    },
+  });
+
+  return (
+    <>
+      {markers.map((marker, i) => {
+        return (
+          <div key={i}>
+            <Marker position={marker.geoCode} icon={new Icon({ iconRetinaUrl: MarkerIcon.src, iconUrl: MarkerIcon.src, shadowUrl: require("leaflet/dist/images/marker-shadow.png") })}>
+              <Popup>{marker.popUp || "No description"}</Popup>
+            </Marker>{" "}
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+const markers = [
+  {
+    geoCode: { lat: -12.99314730803216, lng: -38.514737784862525 },
+    popUp: "Test House1",
+  },
+
+  {
+    geoCode: { lat: -12.991945078416206, lng: -38.51488262414933 },
+    popUp: "Test H 2",
+  },
+
+  {
+    geoCode: { lat: -12.992765731436737, lng: -38.51791352033615 },
+    popUp: "Test Ho 3",
+  },
+];
 
 //icon={new Icon({ iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"), iconUrl: require("leaflet/dist/images/marker-icon.png"), shadowUrl: require("leaflet/dist/images/marker-shadow.png") })}
 
