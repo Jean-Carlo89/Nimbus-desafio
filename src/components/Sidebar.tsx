a948a1b7f8f654a61bbd8e8c169600981a8b0ac4
Jean Carlo <jeancarlodev@gmail.com>
2023-12-20 19:32:18 -0300

    implement json-server integration

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,14 +2,20 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { styled } from "styled-components";
 
 import { FaRegTrashAlt } from "react-icons/fa";
 import MenuLink from "./MenuLink";
+import { Markers } from "@/app/map/layout";
 
 type BarProps = {
   children: React.ReactNode;
+};
+
+export type SideBarProps = {
+  mapData: Markers[];
+  setMapData: Dispatch<SetStateAction<Markers[]>>;
 };
 
 const pontos = [
@@ -23,31 +29,35 @@ const pontos = [
     text: 3,
   },
 ];
-
-export default function SideBar() {
+export default function SideBar({ mapData, setMapData }: SideBarProps) {
   return (
     <div className=" w-full h-full container border-red-500 border-2 flex flex-col justify-between  bg-[#333333] ">
       <div className=" flex flex-col z-10 items-center  border-blue-500 border-4 ">
-        <MenuLink className="py-[10px] block   hover:bg-orange-200 cursor-pointer justify-center h-[90%]  border-pink-500 border-4 w-[80%] mt-[20px] bg-[#D6D8DB]" href={"/map/pontos"}>
+        <MenuLink className="py-[10px] block   hover:bg-orange-200 cursor-pointer justify-center h-[90%]  border-pink-500 border-4 w-[80%] mt-[20px] bg-[#D6D8DB]" href={"/map/pontosIniciais"}>
           Ponto e Zoom Iniciais
         </MenuLink>
 
         <div className="pb-[50px] border-yellow-400 border-2 w-[80%] mt-[20px]">
-          <div className="mx-auto border-red-400 border-2 flex justify-center text-white">
-            Pontos <span className="ml-[10px]">+</span>
+          <MenuLink className="pb-[50px] border-yellow-400 border-2 w-[80%] mt-[20px] z-10" href={"/map/pontos"}>
+            <div className="mx-auto border-red-400 border-2 flex justify-center text-white">
+              Pontos <span className="ml-[10px]">+</span>
+            </div>
+          </MenuLink>
+
+          <div className="overflow overflow-y-scroll max-h-[300px] ">
+            {" "}
+            <ul className="flex flex-col justify-center  w-full   border-green-500 border-4">
+              {mapData.map((ponto, i) => {
+                return (
+                  <div key={ponto.id || i} className="py-[10px] bg-[#D6D8DB] flex justify-between w-full h-[90%]  border-pink-500 border-4 ">
+                    <li>{"Ponto " + (i + 1)}</li>
+
+                    <FaRegTrashAlt />
+                  </div>
+                );
+              })}
+            </ul>
           </div>
-
-          <ul className="flex flex-col justify-center  w-full h-fit border-green-500 border-4">
-            {pontos.map((ponto) => {
-              return (
-                <div key={ponto.text} className="py-[10px] bg-[#D6D8DB] flex justify-between w-full h-[90%]  border-pink-500 border-4 ">
-                  <li key={ponto.text}>{ponto.text}</li>
-
-                  <FaRegTrashAlt />
-                </div>
-              );
-            })}
-          </ul>
         </div>
 
         <div>Circulos</div>
---
+++ src/lib/fetchMarkers.ts
@@ -0,0 +1,20 @@
+// export async function fetchMarkers() {
+//   try {
+//     console.log("here");
+//     const res = await fetch("/api/markers");
+
+//     console.log(res);
+
+//     console.log("here2");
+
+//     const markers = await res.json();
+
+//     return markers;
+//   } catch (e) {
+//     if (e instanceof Error) {
+//       console.log(e.message);
+
+//       return [];
+//     }
+//   }
+// }
